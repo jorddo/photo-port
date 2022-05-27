f7e535705f7445d59297bff8844c975c607d4118
jorddo <jordanbsneed@gmail.com>
2022-05-27 18:38:26 -0500

    switching branches

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
-
   return (
     <div>
       <Nav />
       <main>
-          <About />        
+        <Gallery />
+        <About />
       </main>
     </div>
   );
